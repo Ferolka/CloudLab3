1ba671473835989a11d19e2c9ac2361e7ce5daa9
Simon Boytcov <bsimon20@gmail.com>
2019-02-23 12:50:51 +0200

    commit ahoy

--- src/Cloud.java
+++ src/Cloud.java
@@ -4,6 +4,6 @@ public class Cloud {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Simon krasavchik");
-	}
+	}	System.out.println("Hello Andrei");
 
 }
