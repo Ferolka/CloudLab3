a68b191c18556a7cc69c5f1ac116be24368b83e0
Simon Boytcov <bsimon20@gmail.com>
2019-03-01 14:30:06 +0200

    Resolved merge conflict by incorporating both suggestions.

--- src/Cloud.java
+++ src/Cloud.java
@@ -4,6 +4,7 @@ public class Cloud {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Simon krasavchik");
-	}	System.out.println("Hello Andrei");
+		System.out.println("Hello Andrei");
+	}
 
 }
